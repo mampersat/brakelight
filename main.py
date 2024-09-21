278c3eb2308a40ad01bcb00e45ff1477c64248c4
Matt Sheppard <msheppard@beta.team>
2024-09-21 18:24:23 -0400

    night rider mode working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import machine
+import math
 import neopixel
-import time
 import utime
 
 # Configuration
@@ -36,7 +36,20 @@ def brake_flash():
         brake()
     else:
         off()
+
+def night_rider():
+    cycle_speed_seconds = 1
+    # between zero and 2*pi sin goes from 0 -> 1 -> 0 -> -1 -> 0
+    # so divide a second (or less) into 4 parts
     
+    # cycle from -1 to 1 over a second
+    for i in range(lights):
+        # looks kewl on 4 pixels... adjust for more
+        # should alwyas have at least 1 pixel on, but probably about 1/3 of pixels on at any time
+        s = math.sin(utime.ticks_ms() / 1000 * 2 * math.pi + i) 
+        np[i] = (int(s * 128) + 128, 0, 0)
+    np.write()
+
 def handle_pin_change(pin):
     global mode
     if pin.value() == 1:
@@ -44,14 +57,13 @@ def handle_pin_change(pin):
         mode = brake_flash
     else:
         print("Brake released")
-        mode = cruise
+        mode = night_rider
 
 # Attach interrupt to the pin
 pin.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=handle_pin_change)
 
 # Main loop
-mode = cruise
+mode = night_rider # cruise
 while True:
-
-    # time.sleep(0.1)  # Sleep to reduce CPU usage
+    # time.sleep(0.1)  # Sleep is for the weak
     mode()
