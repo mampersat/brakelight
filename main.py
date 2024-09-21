3a63d27304ee2185f9654f84142e1308267f2162
Matt Sheppard <msheppard@beta.team>
2024-09-21 17:16:30 -0400

    duty cycle + blinking brake lights

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import machine
 import neopixel
 import time
+import utime
 
 # Configuration
 lights = 10
@@ -11,26 +12,46 @@ brake_pin = 27
 pin = machine.Pin(brake_pin, machine.Pin.IN, machine.Pin.PULL_UP)
 np = neopixel.NeoPixel(machine.Pin(neopixel_pin), lights)
 
-def brake():
-    for i in range(lights):
-        np[i] = (255, 0, 0)
-    np.write()
-
 def cruise():
     for i in range(lights):
         np[i] = (64, 0, 0)
     np.write()
 
-def handle_pin_change(pin):
-    if pin.value() == 1:
+def brake():
+    for i in range(lights):
+        np[i] = (255, 0, 0)
+    np.write()
+
+def off():
+    for i in range(lights):
+        np[i] = (0, 0, 0)
+    np.write()
+
+def brake_flash():
+    blinks_per_second = 3
+    blink_interval_ms = 1000 / blinks_per_second  # Calculate the interval in milliseconds
+    time_mod = utime.ticks_ms() % blink_interval_ms
+    print(f"{utime.ticks_ms()} : {time_mod}")
+    if time_mod < (blink_interval_ms / 2):
         brake()
     else:
-        cruise()
+        off()
+    
+def handle_pin_change(pin):
+    global mode
+    if pin.value() == 1:
+        print("Brake pressed")
+        mode = brake_flash
+    else:
+        print("Brake released")
+        mode = cruise
 
 # Attach interrupt to the pin
 pin.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=handle_pin_change)
 
 # Main loop
+mode = cruise
 while True:
-    handle_pin_change(pin)
-    time.sleep(1)  # Sleep to reduce CPU usage
+
+    # time.sleep(0.1)  # Sleep to reduce CPU usage
+    mode()
